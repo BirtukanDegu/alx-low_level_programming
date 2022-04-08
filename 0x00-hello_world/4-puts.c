9ced990d9547b0e327eff384ee09f0ed45300e81
birtukan <birtukandegu2127@gmail.com>
2022-04-08 03:16:07 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - Entry point
  *
- * return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
