56b50fff1cd5bdbfea355255b18c183184d64ed8
birtukan <birtukandegu2127@gmail.com>
2022-04-11 16:37:49 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,11 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX/2;
-printf("last digit of %d is %d", n, n%10);
-if (n % 10 > 5)
-printf("and is greater than 5\n");
-else if (n % 10 == 0)
-printf("and is 0\n");
-else if (n % 10 < 6 && n % 10 !=0)
-printf("and is less than 6 and not 0\n");
+n = rand() - RAND_MAX / 2;
+printf("last digit of %d is %d", n, n % 10,
+((n % 10) == 0) ? "and is 0"
+: (((n % 10) > 5) ? "and is greater than 5"
+: "and is less than 6 and not 9"));
+
 return (0);
 }
