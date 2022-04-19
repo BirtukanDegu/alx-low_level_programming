db10b0cbe45c5a307953cd6c06f2254fe808233a
birtukan <birtukandegu2127@gmail.com>
2022-04-18 18:50:57 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /*
- * reset_to_98 - updates the value it points
+ *reset_to_98 - updates the value it points
  *@n: pointer of the direcction at the variable n
  *Return: 0 (success)
  */
